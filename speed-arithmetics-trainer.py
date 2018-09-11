0cf1db4fe1fc18262756ff64d3dc3961403db4ac
James Chan <jameschanx@gmail.com>
2018-09-10 22:09:15 -0700

    added 4 question types

--- speed-arithmetics-trainer.py
+++ speed-arithmetics-trainer.py
@@ -30,31 +30,41 @@ class Quiz:
         for items in self.categories.items():
             print(items)
     
-    def get_question(q_type):
+    def get_question(self, q_type):
         if q_type == '10to20':
-            np.random.randint(10,20)
-        if q_type == 'firstsamelastsum10':
+            a = np.random.randint(11,20)
+            b = np.random.randint(11,20)
+        elif q_type == 'firstsamelastsum10':
+            first = np.random.randint(1,10)
+            a_second = np.random.randint(1,10)
+            b_second = 10-a_second
+            a = int(str(first) + str(a_second))
+            b = int(str(first) + str(b_second))
+        elif q_type == 'x11':
+            nums=[11]
+            nums.insert(np.random.randint(0,2), np.random.randint(10,1000))
+            a = nums[0]
+            b = nums[1]
+        elif q_type == 'almost100':
+            a = np.random.randint(95,106)
+            b = np.random.randint(95,106)
+        elif q_type == '9series':
             pass
-        if q_type == 'x11':
+        elif q_type == 'twodigit':
             pass
-        if q_type == 'almost100':
+        elif q_type == '3digit':
             pass
-        if q_type == '9series':
+        elif q_type == 'sq_last5':
             pass
-        if q_type == 'twodigit':
+        elif q_type == 'sq_10to20':
             pass
-        if q_type == '3digit':
+        elif q_type == 'sq_almost100':
             pass
-        if q_type == 'sq_last5':
+        elif q_type == 'sq_2digit':
             pass
-        if q_type == 'sq_10to20':
+        elif q_type == 'sq_3digit':
             pass
-        if q_type == 'sq_almost100':
-            pass
-        if q_type == 'sq_2digit':
-            pass
-        if q_type == 'sq_3digit':
-            pass
+        return a,b,a*b
     def show_answers(self):
         pass
         
@@ -78,5 +88,19 @@ class Quiz:
     
 
 if __name__=="__main__":
+    
+#        self.categories = {'10to20':5,
+#                          'firstsamelastsum10':5,
+#                          'x11':2,
+#                          'almost100':5,
+#                          '9series':2,
+#                          'twodigit':10,
+#                          '3digit':10,
+#                          'sq_last5':5,
+#                          'sq_10to20':5,
+#                          'sq_almost100':5,
+#                          'sq_2digit':5,
+#                          'sq_3digit':5}
     quiz = Quiz()
     quiz.show_problems()
+    print(quiz.get_question('almost100'))
