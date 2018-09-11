9a7eb2ccdf774c9d4fbcb61aa708856992b3bb7d
James Chan <jameschanx@gmail.com>
2018-09-10 21:46:36 -0700

    defined basic structure

--- speed-arithmetics-trainer.py
+++ speed-arithmetics-trainer.py
@@ -5,24 +5,37 @@ James Chan
 """
 import numpy as np
 
-
 class Quiz:
-    def __init__(self, num_questions=):
-        self.num_questions = 
-        self.categories = {'10to20':0,
-                          'firstsamelastsum10':0,
-                          'x11':0,
-                          'almost100':0,
-                          '9series':0,
-                          'twodigit':0,
-                          '3digit':0,
-                          'sq_last5':0,
-                          'sq_10to20':0,
-                          'sq_almost100':0,
-                          'sq_2digit':0,
-                          'sq_3digit':0}
+    def __init__(self, num_questions=25):
+#        self.num_questions = num_questions
+#        self.problems = np.zeros((self.num_questions,3))
         
+        self.categories = {'10to20':5,
+                          'firstsamelastsum10':5,
+                          'x11':2,
+                          'almost100':5,
+                          '9series':2,
+                          'twodigit':10,
+                          '3digit':10,
+                          'sq_last5':5,
+                          'sq_10to20':5,
+                          'sq_almost100':5,
+                          'sq_2digit':5,
+                          'sq_3digit':5}
+        
+        self.problems = np.zeros((sum(self.categories.values()),3))
+        self.populate_quiz()
+        
+    def populate_quiz(self):
+        pass
+    
     def show_answers(self):
+        pass
+        
+    def show_problems(self):
+        print(str(self.problems.shape[0]) + ' problems')
+        for i in range(self.problems.shape[0]):
+            print(self.problems[i,:-1])
         
     def add(q_type, num):
         pass
@@ -39,4 +52,6 @@ class Quiz:
     
 
 if __name__=="__main__":
-    print("sup")
+    quiz = Quiz()
+    quiz.show_problems()
+    quiz.show_problems()
