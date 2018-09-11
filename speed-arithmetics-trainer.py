add8686d1813fa6b062982903e153aff34d5a8b9
James Chan <jameschanx@gmail.com>
2018-09-10 21:35:52 -0700

    initial commit'

---
+++ speed-arithmetics-trainer.py
@@ -0,0 +1,42 @@
+"""
+Created on 9/10/18
+
+James Chan
+"""
+import numpy as np
+
+
+class Quiz:
+    def __init__(self, num_questions=):
+        self.num_questions = 
+        self.categories = {'10to20':0,
+                          'firstsamelastsum10':0,
+                          'x11':0,
+                          'almost100':0,
+                          '9series':0,
+                          'twodigit':0,
+                          '3digit':0,
+                          'sq_last5':0,
+                          'sq_10to20':0,
+                          'sq_almost100':0,
+                          'sq_2digit':0,
+                          'sq_3digit':0}
+        
+    def show_answers(self):
+        
+    def add(q_type, num):
+        pass
+    
+    def add_all_concept_equal_prob(self):
+        pass
+        
+    def add_all_concept_rand_prob(self):
+        pass
+
+    def help(self):
+        
+        print("TBD")
+    
+
+if __name__=="__main__":
+    print("sup")
